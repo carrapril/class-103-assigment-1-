9ff9101bfda36f1cf9544667a6750771e7a91ca6
carrapril <97204002+carrapril@users.noreply.github.com>
2022-01-29 10:28:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let firstName="April";
+let lastName="Carr";
+const gender="female";
+const birthDate="4/5/1991"
+let age=30;
+let city="Southfield";
+let email="carrapril@outlook.com";
+let mobileNumber="555-555-5555";
+let homeNumber="123 456 7890";
+let socialNumber="mind your own business, you're not getting my SSN";
+let numberChildren="1 child";
+let childGender="he is a boy";
+const serviceBranch="Air Force";
+let zipCode="12345";
+let country="United States";
+var contactAddress="Prefer to be contacted Cell";
+var vaccineStatus="vaccinated";
+const transportMethod="car";
+let userName="ajcarr";
+let initials="AC"
+//add 2 more variables and list them in document.write under the paragraph
+
+document.write(`All about ${firstName} ${lastName} I am a ${gender}, my birthday is ${birthDate}. I am ${age}.I live in ${city}, ${country}.  My email is ${email}. Here is more random things about me: ${mobileNumber} ${homeNumber} I have one ${numberChildren} ${childGender} I served in the ${serviceBranch} My zipcode is ${zipCode} I represent the ${country} I am ${vaccineStatus}, are you? ${socialNumber} ${contactAddress} ${transportMethod} , my username is ${userName} and my initails are ${initials}.`);
+
+
+
+
